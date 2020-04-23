0cc3b2078f66694794e5bd422ecd073ae21cd8bd
markklik <markklik@gmail.com>
2020-04-23 23:06:21 +0200

    Add Yuta Mori to author field (libdivsufsort)

--- R/package.R
+++ R/package.R
@@ -37,7 +37,7 @@ NULL
 #' `fst` package to facilitate independent updates to the `fstlib`, `LZ4` and `ZSTD` libraries and is used as a
 #' backend to `fst`.
 #'
-#' The fstcore package is based on three C++ libraries:
+#' The fstcore package is based on four C++ libraries:
 #'
 #' * **fstlib**: library containing code to write, read and compute on files stored in the _fst_ format.
 #' Written and owned by Mark Klik.
@@ -45,6 +45,8 @@ NULL
 #' by Yann Collet.
 #' * **ZSTD**: library containing code to compress data with the ZSTD compressor. Written by
 #' Yann Collet and owned by Facebook, Inc.
+#' * **libdivsufsort-lite**: a  lightweight suffix array construction algorithm. This code is bundled with
+#' the ZSTD compression library. Written and owned by Yuta Mori.
 #'
 #'
 #' **_The following copyright notice, list of conditions and disclaimer apply to
@@ -92,7 +94,7 @@ NULL
 #' Redistribution and use in source and binary forms, with or without modification,
 #' are permitted provided that the following conditions are met:
 #'
-#'   * Redistributions of source code must retain the above copyright notice, this
+#' * Redistributions of source code must retain the above copyright notice, this
 #' list of conditions and the following disclaimer.
 #'
 #' * Redistributions in binary form must reproduce the above copyright notice, this
@@ -109,6 +111,56 @@ NULL
 #' ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 #' (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 #' SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#'
+#'
+#' **_The following copyright notice, list of conditions and disclaimer apply to
+#' the use of the fstlib library in the fstcore package:_**
+#' 
+#' fstlib - A C++ library for ultra fast storage and retrieval of datasets
+#'
+#' Copyright (C) 2017-present, Mark AJ Klik
+#'
+#' This file is part of fstlib.
+#'
+#' fstlib is free software: you can redistribute it and/or modify it under the
+#' terms of the GNU Affero General Public License version 3 as published by the
+#' Free Software Foundation.
+#'
+#' fstlib is distributed in the hope that it will be useful, but WITHOUT ANY
+#' WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+#' A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+#' details.
+#'
+#' You should have received a copy of the GNU Affero General Public License
+#' along with fstlib. If not, see <http://www.gnu.org/licenses/>.
+#'
+#'
+#' **_The following copyright notice, list of conditions and disclaimer apply to
+#' the use of the libdivsufsort-lite library in the fstcore package:_**
+#'
+#' Copyright (c) 2003-2008 Yuta Mori All Rights Reserved.
+#'
+#' Permission is hereby granted, free of charge, to any person
+#' obtaining a copy of this software and associated documentation
+#' files (the "Software"), to deal in the Software without
+#' restriction, including without limitation the rights to use,
+#' copy, modify, merge, publish, distribute, sublicense, and/or sell
+#' copies of the Software, and to permit persons to whom the
+#' Software is furnished to do so, subject to the following
+#' conditions:
+#'
+#' The above copyright notice and this permission notice shall be
+#' included in all copies or substantial portions of the Software.
+#'
+#' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+#' EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+#' OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+#' NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+#' HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+#' WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+#' FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+#' OTHER DEALINGS IN THE SOFTWARE.
+#'
 #'
 #' @md
 #' @docType package
